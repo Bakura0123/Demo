410db97e5055fc4d797d19571e0e5c25f3be9fd9
PhamKhacHuy <khachuy966@gmail.com>
2018-07-26 19:05:03 +0700

    hi

---
+++ ReactJS/myapp3/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Login_form from './Login-form';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Login_form/>
+      </div>
+    );
+  }
+}
+
+export default App;
