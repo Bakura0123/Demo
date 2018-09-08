f805f4824d35133f11d7210e8af5ca1a69c806b8
Bakura0123 <34256879+Bakura0123@users.noreply.github.com>
2018-09-08 16:26:13 +0700

    8/9

--- ReactJS/myapp3/src/huy.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './huy.css';
-import registerServiceWorker from './registerServiceWorker';
-const Welcome=(props)=> { return <h1>Hello,{props.name}</h1>; }
-const element = <Welcome name ="Sara"/>;
-ReactDOM.render(element,document.getElementById('root'));
-registerServiceWorker();
